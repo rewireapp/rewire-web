e1491042b2f5db09b86dc6817adb62f568043d8e
Matt Stillwell <stillwell006@gmail.com>
2021-04-16 15:12:49 -0400

    🎉 Inital ts react material webapp

---
+++ src/components/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+import Navbar from './Navbar';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
